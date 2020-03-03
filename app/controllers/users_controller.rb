a9aacff46e40b9381fad9e8eab2eb6c2fac01d0e
sato chiharu <kujira028@gmail.com>
2020-03-03 03:00:42 +0000

    [Finish] 5-5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
   	# ユーザーデータを一件取得しインスタンス変数へ渡す
   	@user = User.find(params[:id])
+  	# @userに関連づけた投稿(.post_image)のみ@post_imagesに渡すことができます。
+  	@post_images = @user.post_images.page(params[:page]).reverse_order
   end
 end
