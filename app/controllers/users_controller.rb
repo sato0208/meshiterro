4f207f5d9399862d3c6648ca5f16c695cfbeb976
sato chiharu <kujira028@gmail.com>
2020-03-03 09:02:02 +0000

    [Finish] 5-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,24 @@ class UsersController < ApplicationController
   	# @userに関連づけた投稿(.post_image)のみ@post_imagesに渡すことができます。
   	@post_images = @user.post_images.page(params[:page]).reverse_order
   end
+
+  def edit
+  	# ユーザーデータを一件取得しインスタンス変数へ渡す。
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	# 変更結果を保存できるようにする
+  	# ユーザーデータを一件取得しインスタンス変数へ渡す。
+  	@user = User.find(paramsk[:id])
+  	# 編集したものをセーブする
+  	@user.update(user_params)
+  	# user_pathへリダイレクトする
+  	redirect_to user_path(@user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,11 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show] do
-   	resource :post_comments, only: [:create]
-  end
-
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
       resource :favorites, only: [:create, :destroy]
       resource :post_comments, only: [:create, :destroy]
-   end
 
-   # showアクションのみなので不必要なルーティングが生成されるのを制限
-   resources :users, only:[:show]
+  end
+
+  resources :users, only: [:show, :edit, :update]
 end
